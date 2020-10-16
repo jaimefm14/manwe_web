5578a70905aa9af0f1c02dd9b86e0e32a6ca69fb
jaimefm14 <jaimefrm93@gmail.com>
2020-10-16 09:23:46 +0200

    Solucionado problema con el footer y campos de texto del contacto alineados

--- src/components/form/contacto.js
+++ src/components/form/contacto.js
@@ -9,13 +9,6 @@ import arrowL from '../../asserts/images/arrow_l.png';
 
 import Footer from '../footer/Footer';
 
-var top = {
-  paddingLeft: `5vw`,
-  paddingTop: `5vw`,
-  fontFamily: `Poppins`,
-  fontSize: `bold`
-}
-
 const trackingId = "UA-30843009-3";
 ReactGA.initialize(trackingId);
 ReactGA.pageview(window.location.pathname + window.location.search);
@@ -33,63 +26,47 @@ class form extends React.Component {
   }
   render() {
     return (
-      <>
-
-        <Container>
-
-          <Row style={top}>
-            <Col>
+      <Container fluid="true">
+          <Row>
+            <Col md={2}>
               <Link to="/"><img src={arrowL} alt="Flecha" class="Flecha" /></Link>
             </Col>
-            <Col md={{ span: 9, offset: 1 }}>{` `}</Col>
-            <Col><p class="titletop">Contacto</p></Col>
+            <Col md={{span:1, offset:9}}><p class="titletop">Contacto</p></Col>
           </Row>
-
-        </Container>
-
-        <Container>
-
-          <Form>
-            <Form.Group as={Col} controlId="formGrid-Nombre">
-              <Form.Label class="titlecontacta">Nombre</Form.Label>
-              <Form.Control type="Nombre" placeholder="Nombre" />
-            </Form.Group>
-
-            <Row>
-              <Form.Group controlId="formGrid-mail">
-                <Form.Label class="titlecontacta">Email</Form.Label>
-                <Form.Control placeholder="Email" />
+          <Row>
+            <Form>
+              <Form.Group controlId="formGrid-Nombre">
+                <Form.Label className="">Nombre</Form.Label>
+                <Form.Control type="Nombre" placeholder="Nombre" />
               </Form.Group>
-              <Col md={{ span: 1, offset: 1 }}>{` `}</Col>
-              <Form.Group controlId="formGrid-telefono" >
-                <Form.Label class="titlecontacta">Teléfono</Form.Label>
-                <Form.Control placeholder="Teléfono" />
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGrid-mail">
+                  <Form.Label class="">Email</Form.Label>
+                  <Form.Control placeholder="Email" />
+                </Form.Group>
+                <Form.Group as ={Col} controlId="formGrid-telefono" >
+                  <Form.Label class="">Teléfono</Form.Label>
+                  <Form.Control placeholder="Teléfono" />
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group controlId="formGrid-mensaje">
+                <Form.Label class="">Mensaje</Form.Label>
+                <Form.Control as="textarea" rows="4" placeholder="Tu mensaje" />
               </Form.Group>
-            </Row>
-
-            <Form.Group controlId="formGrid-mensaje">
-              <Form.Label class="titlecontacta">Mensaje</Form.Label>
-              <Form.Control as="textarea" rows="4" placeholder="Tu mensaje" />
-            </Form.Group>
-
-            <button class="button1" onClick={sendMail}>
-              Enviar
-            </button>
-
-          </Form>
-
+              <button class="button1" onClick={sendMail}>
+                Enviar
+              </button>
+            </Form>
+          </Row>
           <div id="popUp" class="alert alert-success alert-dismissible fade show" role="alert" style={{ display: 'none' }}>
             <strong>¡Mensaje enviado!</strong> Pronto nos pondremos en contacto contigo.
             <button type="button" class="close" data-dismiss="alert" aria-label="Close" fontFamily="Poppins">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-
-        </Container>
-
-        <Footer></Footer>
-
-      </>
+          <Footer></Footer>
+      </Container>
     )
   }
 }
