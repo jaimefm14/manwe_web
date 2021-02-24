9ba51285847106721858565da4f8db6f7a1a1656
Marcos <marcosspkcod@gmail.com>
2021-02-24 13:25:43 +0100

    apaño footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 import logo from "../../asserts/images/Logo_Manwë.png";
 import facebook from "../../asserts/images/facebook.svg";
-import instagram from "../../asserts/images/instagram.svg";
+import instagram from "../../asserts/images/Instagram.svg";
 import linkedin from "../../asserts/images/linkedin.svg";
 import twitter from "../../asserts/images/twitter.svg";
 
@@ -30,46 +30,48 @@ class Footer extends React.Component {
     render() {
         return (
             <>
-            <Container id="footer" fluid="true">
-                <Row className="justify-content-md-center">
-                    <Col className="col-5">
-                        <p style={title}>Contacto</p>
-                        <a href="mailto:info@manwe.tech"><p style={text}>info@manwe.tech</p></a>
+                <Container id="footer" fluid="true">
+                    <Row className="justify-content-md-center">
+                        <Col className="col-5">
+                            <p style={title}>Contacto</p>
+                            <a href="mailto:info@manwe.tech"><p style={text}>info@manwe.tech</p></a>
 
-                        <p style={title}>Teléfono</p>
-                        <a href="tel:+34687155736"><p style={text}>+34 687155736</p></a>
+                            <p style={title}>Teléfono</p>
+                            <a href="tel:+34 687155736"><p style={text}>+34 687155736</p></a>
 
-                        <p style={title}>Sede</p>
+                            {/*<p style={title}>Sede</p>
                         <a href="http://maps.google.com/?q=Ciudad Politécnica de la Innovación (CPI) Edificio 8B Acceso N 4ª Planta, 46022 Valencia" 
                             target='_blank' rel="noopener noreferrer">
                             <p style={text}>C\ Vera s/n, 46021 Valencia, España</p>
-                        </a>
-                    </Col>
-                    <Col className="col-5">
-                        <p style={title}>Políticas</p>
-                        <Link to="politica-de-privacidad"><p style={text}>Políticas de privacidad</p></Link>
-                        <Link to="politica-de-cookies"><p style={text}>Política de cookies</p></Link>
-                        <Link to="#"><p style={text}>Términos de uso</p></Link>
-                        <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
-                            <img className="socialNetworks" src={linkedin} alt="Linkedin"/>
-                        </a>
-                        <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
+                        </a>*/}
+                        
+                            <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
+                                <img className="linkedin" src={linkedin} alt="Linkedin" />
+                            </a>
+                            {/*<a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
                             <img className="socialNetworks" src={facebook} alt="Facebook"/>
-                        </a>
-                        <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
+                        </a>*/}
+                            {/*<a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
                             <img className="socialNetworks" src={twitter} alt="Twitter"/>
-                        </a>
-                        <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
-                            <img className="socialNetworks" src={instagram} alt="Instagram"/>
-                        </a>
-                    </Col>
-                    <Col className="col-2">
-                        <p style={title}>FAQ</p>
-                        <Link to="/contacto"><p style={text}>¿Tienes dudas?</p></Link>
-                        <Link to="/"><img src={logo} alt="logo" className="img"/></Link>
-                    </Col>
-                </Row>
-            </Container>
+                        </a>*/}
+                            <a href="https://www.linkedin.com/company/manwë" target="_blank" rel="noopener noreferrer">
+                                <img className="socialNetworks" src={instagram} alt="Instagram" />
+                            </a>
+                        </Col>
+                        <Col className="col-5">
+                            <p style={title}>Políticas</p>
+                            <Link to="politica-de-privacidad"><p style={text}>Políticas de privacidad</p></Link>
+                            <Link to="politica-de-cookies"><p style={text}>Política de cookies</p></Link>
+                            <Link to="#"><p style={text}>Términos de uso</p></Link>
+
+                        </Col>
+                        <Col className="col-2">
+                            <p style={title}>FAQ</p>
+                            <Link to="/contacto"><p style={text}>¿Tienes dudas?</p></Link>
+                            <Link to="/"><img src={logo} alt="logo" className="img" /></Link>
+                        </Col>
+                    </Row>
+                </Container>
             </>
         )
     }
